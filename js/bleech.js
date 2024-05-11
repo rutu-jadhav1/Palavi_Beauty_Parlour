695e24d987aa74db418fe44610d673637e620d39
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-05-11 15:12:34 +0530

    bleech.html page

---
+++ js/bleech.js
@@ -0,0 +1,10 @@
+function changeImage(imageName){
+    const mainImg = document.getElementById('mainProductImage');
+    mainImg.src = imageName;
+}
+
+function bookbtn()
+{
+    document.getElementById('bookbtn');
+    alert('Thanks for Booking!')
+}
