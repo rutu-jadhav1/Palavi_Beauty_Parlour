e4841557ff7ecf3f9d7e27716892f227d3bbaebb
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-05-17 16:09:25 +0530

    js for all work image applied

--- js/index.js
+++ js/index.js
@@ -8,3 +8,15 @@ function closeModal()
 {
     document.getElementById('myModal').style.display = "none";   
 }
+
+function showWork(id)
+{
+    var workContent = document.getElementById(id);
+    var modalcontent = document.getElementById('modal-content1');
+    modalcontent.innerHTML = workContent.innerHTML;
+    document.getElementById('myModal1').style.display = "block";
+}
+function closeModal1()
+{
+    document.getElementById('myModal1').style.display = "none";   
+}
